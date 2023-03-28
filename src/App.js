c5381562edf22bd59d09dca5969d0876e43270cb
jeannie <jeanniezheng123@gmail.com>
2023-03-28 14:28:07 -0500

    Fetch forecast api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css'
 // import gif from './assets/cloudy2.gif';
 
@@ -12,37 +12,56 @@ function App() {
   const currTime = new Date().toLocaleTimeString();
 
 
+
   const inputLocation = (event) => {
     event.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
     fetch(url)
-      .then(response => {
+      .then((response) => {
         if (!response.ok) {
           throw new Error("City not found");
         }
-        console.log(response)
+        console.log(response);
         return response.json();
       })
-      .then(results => {
+      .then((results) => {
         setData(results);
         setCity(location.toUpperCase());
-        setBackgroundImage(`../assets/${results.weather[0].icon === '50d' ? '50d' : results.weather[0].main}.gif`)
-        setLocation('');
+        setBackgroundImage(
+          `../assets/${results.weather[0].icon === "50d"
+            ? "50d"
+            : results.weather[0].main
+          }.gif`
+        );
+        setLocation("");
         event.target.city.blur();
+        console.log('LAT' + results.coord.lat)
+        console.log('LONG' + results.coord.lon)
+        fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${results.coord.lat}&lon=${results.coord.lon}&appid=8c0b474f6453fd3a3342808b8c1166c5`
+        )
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Error fetching forecast data");
+            }
+            console.log(response.json());
+            return response.json();
+          })
+          .catch((error) => {
+            console.error(error.message);
 
-
+          });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error.message);
-        let randomGif = Math.floor(Math.random() * 3) + 1
+        let randomGif = Math.floor(Math.random() * 3) + 1;
         setData({});
-        setLocation('');
-        setCity('LOCATION NOT FOUND!');
-        setBackgroundImage(`../assets/Error${randomGif}.gif`)
+        setLocation("");
+        setCity("LOCATION NOT FOUND!");
+        setBackgroundImage(`../assets/Error${randomGif}.gif`);
         event.target.city.blur();
       });
-    console.log(data)
-
+    console.log(data);
   };
 
 
