bbf3393cdb129ceb4e136a0d0fe68f8b4129f818
jeannie <jeanniezheng123@gmail.com>
2023-03-28 15:09:25 -0500

    Change Promises to async and await

--- src/App.js
+++ src/App.js
@@ -8,62 +8,57 @@ function App() {
   const [location, setLocation] = useState('')
   const [city, setCity] = useState("Today's Weather")
   const [backgroundImage, setBackgroundImage] = useState('../assets/choose.gif')
+  const [forecast, setForecast] = useState({})
   // const currDate = new Date().toLocaleDateString();
   const currTime = new Date().toLocaleTimeString();
+  //create state for an array of an array 
 
 
+  useEffect(() => {
+    console.log(forecast);
+  }, [forecast]);
 
-  const inputLocation = (event) => {
+  const inputLocation = async (event) => {
     event.preventDefault();
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("City not found");
-        }
-        console.log(response);
-        return response.json();
-      })
-      .then((results) => {
-        setData(results);
-        setCity(location.toUpperCase());
-        setBackgroundImage(
-          `../assets/${results.weather[0].icon === "50d"
-            ? "50d"
-            : results.weather[0].main
-          }.gif`
-        );
-        setLocation("");
-        event.target.city.blur();
-        console.log('LAT' + results.coord.lat)
-        console.log('LONG' + results.coord.lon)
-        fetch(
-          `https://api.openweathermap.org/data/2.5/forecast?lat=${results.coord.lat}&lon=${results.coord.lon}&appid=8c0b474f6453fd3a3342808b8c1166c5`
-        )
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error("Error fetching forecast data");
-            }
-            console.log(response.json());
-            return response.json();
-          })
-          .catch((error) => {
-            console.error(error.message);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+      console.log(response);
+      const results = await response.json();
+      setData(results);
+      setCity(location.toUpperCase());
+      setBackgroundImage(
+        `../assets/${results.weather[0].icon === "50d"
+          ? "50d"
+          : results.weather[0].main
+        }.gif`
+      );
+      setLocation("");
+      event.target.city.blur();
 
-          });
-      })
-      .catch((error) => {
-        console.error(error.message);
-        let randomGif = Math.floor(Math.random() * 3) + 1;
-        setData({});
-        setLocation("");
-        setCity("LOCATION NOT FOUND!");
-        setBackgroundImage(`../assets/Error${randomGif}.gif`);
-        event.target.city.blur();
-      });
+      const forecastResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${results.coord.lat}&lon=${results.coord.lon}&appid=8c0b474f6453fd3a3342808b8c1166c5`
+      );
+      if (!forecastResponse.ok) {
+        throw new Error("Error fetching forecast data");
+      }
+      const forecastResults = await forecastResponse.json();
+      console.log(forecastResults);
+      setForecast(forecastResults);
+    } catch (error) {
+      console.error(error.message);
+      let randomGif = Math.floor(Math.random() * 3) + 1;
+      setData({});
+      setLocation("");
+      setCity("LOCATION NOT FOUND!");
+      setBackgroundImage(`../assets/Error${randomGif}.gif`);
+      event.target.city.blur();
+    }
     console.log(data);
   };
-
 
   let handleChange = (e) => {
     setLocation(e.target.value)
@@ -102,6 +97,12 @@ function App() {
           <h1>{city} | {currTime}</h1>
           <div className='temp'>
             {data.main && (convertToFahrenheit(data.main.temp))}
+            {/* {forecast && forecast.map((item) => (
+              <div key={item.dt}>
+                <p>{item.dt_txt}</p>
+                <p>{item.main.temp}</p>
+              </div>
+            ))} */}
             <h2>{data.main && data.weather[0].description}</h2>
           </div>
         </div>
